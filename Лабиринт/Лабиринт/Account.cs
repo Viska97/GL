30082c71d83d2a8762fa8a579b4acf3a57d1b55c
DanMus95 <19dani95@mail.ru>
2017-04-21 22:07:24 +0300

    21.04.2017
    мы чето намутили

---
+++ Лабиринт/Лабиринт/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лабиринт
+{
+    public partial class Account : Form
+    {
+        login login;
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        public Account(login login)
+        {
+            this.login = login;
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Account_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            login.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Лабиринт/Лабиринт/login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лабиринт
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            RegistrationForm registration = new RegistrationForm(this);
+            registration.ShowDialog();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            Account account = new Account(this);
+            account.ShowDialog();
+        }
+    }
+}
