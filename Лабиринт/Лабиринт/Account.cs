48c186e5e75638401c6032923da1ebfbe50c7d19
VasilyMaterikin <m.vis97@mail.ru>
2017-05-10 15:22:28 +0300

    Интеграция лабиринта 2

--- Лабиринт/Лабиринт/Account.cs
+++ Лабиринт/Лабиринт/Account.cs
@@ -47,7 +47,7 @@ namespace Лабиринт
         private void button3_Click(object sender, EventArgs e)
         {
             this.Visible = false;
-            Labyrinth labyrinth = new Labyrinth(this, 41, 41, 12, false, 20);
+            Labyrinth labyrinth = new Labyrinth(this, 41, 41, 12, true, 20);
             labyrinth.ShowDialog();
         }
 
