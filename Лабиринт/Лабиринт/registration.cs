4aa837bc2d4b8e0297fe5beac8e65743157af135
AriyWariy <Ariy_tale@mail.ru>
2017-05-16 00:57:57 +0300

    поключил sqlite

--- Лабиринт/Лабиринт/registration.cs
+++ Лабиринт/Лабиринт/registration.cs
@@ -6,12 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.SQLite;
 
 namespace Лабиринт
 {
     public partial class RegistrationForm : Form
     {
-        login login;
+               login login;
         public RegistrationForm()
         {
             InitializeComponent();
@@ -37,6 +39,25 @@ namespace Лабиринт
         private void Form2_FormClosed(object sender, FormClosedEventArgs e)
         {
             login.Visible = true;
+        }
+
+        private void zaregistrirovtsy_Click(object sender, EventArgs e)
+        {
+            string lastname = Convert.ToString(textBox1.Text);
+            string firstname = Convert.ToString(textBox2.Text);
+            string otchestvo = Convert.ToString(textBox3.Text);
+            string password = Convert.ToString(textBox4.Text);
+            string logins = lastname + firstname;
+            if (radioButton1.Checked)
+                { int type = 1; }
+            else { int type = 0; }
+            const string databaseName = @"C:\Users\Ariy\Documents\Visual Studio 2015\Projects\labirinth\GL\labirinth.db";
+            SQLiteConnection connection =
+                new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand("INSERT INTO 'Accounts' ('lastname', 'firstname', 'otchestvo', 'password', 'login', 'type') VALUES (lastname, firstname, otchestvo, password, logins, type);", connection);
+            command.ExecuteNonQuery();
+            connection.Close();                 
         }
     }
 }
